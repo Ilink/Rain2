c2b77da618bf74fb0157f15808317b107299aaac
U-Angler\Ian <Ian@Angler.(none)>
2013-01-27 01:49:24 -0800

    removed old files

--- Rain/geo.cpp
+++
@@ -1,8 +0,0 @@
-#include "geo.h"
-
-Geo::Geo(vector <GLfloat>& verts, vector<int>& triIndex){
-	this->triIndex = triIndex;
-	this->verts = verts;
-	glGenBuffers(1, this->vboGeo);
-	glBindBuffer(GL_ARRAY_BUFFER, gVBO);
-};
