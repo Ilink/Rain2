49e1d89f091e6874ce8ce3b01998b898a4e07d32
ilink <ian@linkportfolio.net>
2013-01-30 00:43:08 -0800

    transformation component

---
+++ Rain/components/transComponent.h
@@ -0,0 +1,29 @@
+#ifndef TRANS_COMPONENT_H
+#define TRANS_COMPONENT_H
+
+#include <GL/glew.h>
+#include "../include/Artemis-Cpp/Component.h"
+// #include <Artemis-Cpp/Component.h>
+#include <vector>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+class TransComponent : public artemis::Component{
+public:
+    glm::mat4 trans;
+
+    TransComponent(glm::mat4& trans){
+        this->trans = trans;
+    };
+
+    TransComponent(){
+        this->trans = glm::mat4(1.0f); // identity
+    };
+    
+    ~TransComponent(){
+
+    };
+};
+
+#endif
