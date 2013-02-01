ceacb900ccabc78a9fe6d9fac41a28981e6d272a
Ian Link <ian@linkportfolio.net>
2013-01-31 16:48:10 -0800

    notes

--- Rain/types.h
+++ Rain/types.h
@@ -4,9 +4,9 @@
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 struct vertex {
-    GLfloat x, y, z;        //Vertex
-    GLfloat nx, ny, nz;     //Normal
-    GLfloat u, v;         //Texcoord
+    GLfloat x, y, z;        // Vertex
+    GLfloat nx, ny, nz;     // Normal
+    GLfloat u, v;           // Texcoord
 };
 
 struct Position {
@@ -17,6 +17,7 @@ struct Color {
 	GLfloat x, y, z;
 };
 
+// i think we need more of these, but they are unused right now
 #define POSATTR 0
 #define MVPUNI 1
 
