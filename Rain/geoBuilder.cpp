3a032b35a7a10a69b528296caf270dfaeeebcdf6
U-Angler\Ian <Ian@Angler.(none)>
2013-02-05 00:48:29 -0800

    moving on to shadowsS

--- Rain/geoBuilder.cpp
+++ Rain/geoBuilder.cpp
@@ -14,89 +14,53 @@ void makeBox(double height, double width, double depth, vector <vertex>& verts, 
     verts.resize(24);
     triIndex.resize(36);
 
+    double left = -width/2.0;
+    double right = width/2.0;
+    double top = height/2.0;
+    double bottom = -height/2.0;
+    double close = -depth/2.0;
+    double far = depth/2.0;
+
     // Front
-    verts[0].x = 0.0;           verts[0].y = 0.0;           verts[0].z = 0.0;
-    verts[1].x = width;         verts[1].y = 0.0;           verts[1].z = 0.0;
-    verts[2].x = width;         verts[2].y = height;        verts[2].z = 0.0;
-    verts[3].x = 0.0;           verts[3].y = height;        verts[3].z = 0.0;
+    verts[0].x = left;          verts[0].y = bottom;           verts[0].z = close;
+    verts[1].x = right;         verts[1].y = bottom;           verts[1].z = close;
+    verts[2].x = right;         verts[2].y = top;              verts[2].z = close;
+    verts[3].x = left;          verts[3].y = top;              verts[3].z = close;
     
     // Back
-    verts[4].x = 0.0;         verts[4].y = 0.0;           verts[4].z = depth;
-    verts[5].x = 0.0;           verts[5].y = height;           verts[5].z = depth;
-    verts[6].x = width;           verts[6].y = height;        verts[6].z = depth;
-    verts[7].x = width;         verts[7].y = 0.0;           verts[7].z = depth;
+    verts[4].x = left;          verts[4].y = bottom;           verts[4].z = far;
+    verts[5].x = left;          verts[5].y = top;              verts[5].z = far;
+    verts[6].x = right;         verts[6].y = top;              verts[6].z = far;
+    verts[7].x = right;         verts[7].y = bottom;           verts[7].z = far;
 
     // Top
-    verts[8].x = 0.0;           verts[8].y = height;        verts[8].z = depth;
-    verts[9].x = 0.0;           verts[9].y = height;        verts[9].z = 0.0;
-    verts[10].x = width;        verts[10].y = height;       verts[10].z = 0.0;
-    verts[11].x = width;        verts[11].y = height;       verts[11].z = depth;
+    verts[8].x = left;           verts[8].y = top;             verts[8].z = close;
+    verts[9].x = left;           verts[9].y = top;             verts[9].z = far;
+    verts[10].x = right;         verts[10].y = top;            verts[10].z = far;
+    verts[11].x = right;         verts[11].y = top;            verts[11].z = close;
 
     // Bottom
-    verts[12].x = 0.0;          verts[12].y = 0.0;          verts[12].z = depth;
-    verts[13].x = width;        verts[13].y = 0.0;          verts[13].z = depth;
-    verts[14].x = width;        verts[14].y = 0.0;       verts[14].z = 0.0;
-    verts[15].x = 0.0;          verts[15].y = 0.0;          verts[15].z = 0.0;
+    verts[12].x = left;          verts[12].y = bottom;          verts[12].z = close;
+    verts[13].x = right;        verts[13].y = bottom;          verts[13].z = close;
+    verts[14].x = right;        verts[14].y = bottom;           verts[14].z = far;
+    verts[15].x = left;          verts[15].y = bottom;          verts[15].z = far;
 
     // Right
-    verts[16].x = width;        verts[16].y = 0.0;          verts[16].z = depth;
-    verts[17].x = width;        verts[17].y = height;       verts[17].z = depth;
-    verts[18].x = width;        verts[18].y = height;       verts[18].z = 0.0;
-    verts[19].x = width;        verts[19].y = 0.0;          verts[19].z = 0.0;
+    verts[16].x = right;        verts[16].y = bottom;          verts[16].z = far;
+    verts[17].x = right;        verts[17].y = top;       verts[17].z = far;
+    verts[18].x = right;        verts[18].y = top;       verts[18].z = close;
+    verts[19].x = right;        verts[19].y = bottom;          verts[19].z = close;
 
     // Left
-    verts[20].x = 0.0;          verts[20].y = 0.0;          verts[20].z = 0.0;
-    verts[21].x = 0.0;          verts[21].y = 0.0;          verts[21].z = depth;
-    verts[22].x = 0.0;          verts[22].y = height;       verts[22].z = depth;
-    verts[23].x = 0.0;          verts[23].y = height;       verts[23].z = 0.0;
-
-    // // Front face
-    // 0.0, 0.0,  depth,
-    // width, 0.0,  depth,
-    // width,  height,  depth,
-    // 0.0,  height,  depth,
-    
-    // // Back face
-    // 0.0, 0.0, 0.0,
-    // 0.0,  height, 0.0,
-    // width,  height, 0.0,
-    // width, 0.0, 0.0,
-
-    // // Top face
-    // 0.0, height, 0.0,
-    // 0.0, height,  depth,
-    // width, height,  depth,
-    // width, height, 0.0,
-
-    // // Bottom face
-    // 0.0, 0.0, 0.0,
-    // width, 0.0, 0.0,
-    // width, 0.0,  depth,
-    // 0.0, 0.0,  depth,
-
-    // // Right face
-    // width, 0.0, 0.0,
-    // width, height, 0.0,
-    // width, height,  depth,
-    // width, 0.0,  depth,
-
-    // // Left face
-    // 0.0, 0.0, 0.0,
-    // 0.0, 0.0,  depth,
-    // 0.0,  height,  depth,
-    // 0.0,  height, 0.0
+    verts[20].x = left;          verts[20].y = bottom;          verts[20].z = close;
+    verts[21].x = left;          verts[21].y = bottom;          verts[21].z = far;
+    verts[22].x = left;          verts[22].y = top;       verts[22].z = far;
+    verts[23].x = left;          verts[23].y = top;       verts[23].z = close;
 
 
-    //     0,1,2,      0,2,3,      // front
-    //     4,5,6,      4,6,7,      // back
-    //     8,9,10,     8,10,11,    // top
-    //     12,13,14,   12,14,15,   // bottom
-    //     16,17,18,   16,18,19,   // right
-    //     20,21,22,   20,22,23    // left
-
-    // 0, 1, 2,
-    // 2, 1, 3
-
+    /*
+    the vertex index should be flipped, not the verts?
+    */
     triIndex[0] = 0;    triIndex[1] = 1;    triIndex[2] = 2;
     triIndex[3] = 0;    triIndex[4] = 2;    triIndex[5] = 3;
 
---
+++ Rain/systems/shadowSystem.cpp
@@ -0,0 +1,2 @@
+#include "shadowSystem.h"
+
