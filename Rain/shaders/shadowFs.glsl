56a32cc0bec0c6543abc81d9aa26ccad06f6e835
U-Angler\Ian <Ian@Angler.(none)>
2013-02-06 00:18:01 -0800

    progresss on shadow fs

--- Rain/shaders/shadowFs.glsl
+++ Rain/shaders/shadowFs.glsl
@@ -1,10 +1,8 @@
-varying vec3 vLightWeighting;
 varying vec4 vPosition;
+uniform sampler2DShadow shadowMap;
+varying vec4 ShadowCoord;
 
-void main(void) {
-    vec3 color = vec3(0.8, 0.4, 0.1);
-    vec3 specColor = vec3(0.1,0.5, 0.2);
-    vec3 diffuseColor = vec3(0.2, 0.1, 0.2);
-
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+void main() {     
+    float shadow = textureProj(shadowMap, ShadowCoord);
+    gl_FragColor = vec4(shadow, 1.0);
 }
