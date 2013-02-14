dc86bea52827bd2a63ce2ab326a83b9ba5b1940b
U-Angler\Ian <Ian@Angler.(none)>
2013-02-13 22:07:14 -0800

    progress?

--- Rain/shaders/shadowFs.glsl
+++ Rain/shaders/shadowFs.glsl
@@ -5,7 +5,7 @@ varying vec4 vShadowCoord;
 
 void main() {
     // gl_FragColor = texture2D(uShadowmapSampler, vShadowCoord.st);
-    float shadow = shadow2DProj(uShadowmapSampler, vShadowCoord).z;
+    float shadow = textureProj(uShadowmapSampler, vShadowCoord);
     // float shadow = textureProj(uShadowmapSampler, vShadowCoord);
     gl_FragColor = vec4(shadow, shadow, shadow, 1.0);
 }
