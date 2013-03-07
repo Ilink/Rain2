6fee0e4a943942cce14554c1579df4954ab773bd
Ian Link <ian@linkportfolio.net>
2013-03-06 16:23:03 -0800

    notes

--- Rain/shaders/shadowVs.glsl
+++ Rain/shaders/shadowVs.glsl
@@ -13,6 +13,8 @@ void main(void) {
     vShadowCoord = uShadowMatrix * vec4(pos, 1.0);
     gl_Position = uPMatrix * uMVMatrix * vec4(pos, 1.0);
     // uv = (vec2( gl_Position.x, gl_Position.y ) + vec2( 1.0 ) ) * 0.5;
-    uv = vec2(gl_Position.xy * 0.5 + 0.5);
+    uv = vec2(gl_Position.xy * 0.5 + 0.5); // transform to from -1..1 to 0..1
+    									   // clip space is going to be -1..1 whereas texture space is 0..1
+    									   // vShadowCoord doesnt need this because it was premultipied by a matrix which handles the conversion
     vNormal = normal;
 }
